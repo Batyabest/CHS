09dd58135691e4f49c2600904bcff48e39e40825
Batyabest <batyamail@yandex.ru>
2017-02-26 18:01:13 +0300

    Сделан функционал загрузки фото. Также путь к фото прописывается в БД.

--- core/components/chs/processors/web/create.class.php
+++ core/components/chs/processors/web/create.class.php
@@ -7,13 +7,51 @@ class chsFizikCreateProcessor extends modObjectCreateProcessor
     public $languageTopics = array('chs');
     //public $permission = 'create';
 
-
-
     /**
      * @return bool
      */
+
+
     public function beforeSet()
     {
+
+        // добавляем загрузку файла
+        if (!empty($_FILES['photo']['tmp_name'])) {
+            $source = $this->modx->getObject('sources.modMediaSource', 1);
+            $source->initialize();
+            $file_dir = 'files/' . $_POST['uid'] . '/'; // Путь к файлу
+            $dirs = explode('/', $file_dir);
+            $path = $this->modx->getOption('base_path') . trim($this->modx->getOption('assets_url'), '/');
+            foreach ($dirs as $dir) {
+                $path = $path . '/' . trim($dir, '/');
+                if (!file_exists($path)) {
+                    mkdir($path);
+                }
+            }
+            foreach ($_FILES as $key => $file) {
+                $tmp = explode('.', $file['name']);
+                $ext = array_pop($tmp);
+                $filename = md5(time()) . '.' . $ext; // Имя файла
+                $_FILES[$key]['name'] = $filename;
+                $size = $_FILES[$key]['size'];
+            }
+            $this->setProperty('string1', str_replace($this->modx->getOption('base_path'), '/', $path) . $filename);
+            if (!$source->uploadObjectsToContainer($this->modx->getOption('assets_url') . $file_dir, $_FILES)) {
+                foreach ($source->errors as $error) {
+                    $this->modx->lexicon->load('core:file');
+                    $this->addFieldError('string1', $this->modx->lexicon($error, array(
+                        'ext' => $ext,
+                        'size' => $size,
+                        'allowed' => $this->modx->getOption('upload_maxsize')
+                    )));
+                }
+            }
+        }
+        // конец
+
+        $this->setProperty('image', '/assets/' . $file_dir.$filename);
+
+
         $name = trim($this->getProperty('name'));
         if (empty($name)) {
             $this->modx->error->addField('name', $this->modx->lexicon('chs_fizik_err_name'));
